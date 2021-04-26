c5bf31027bbd313b45c79d20c3007e8a033aba02
dvirainisch <63093025+dvirainisch@users.noreply.github.com>
2021-04-26 16:37:45 +0300

    dss

--- settings.js
+++ settings.js
@@ -1,36 +1,4 @@
-function KeyBoards() {
-    //todo
-}
 
-
-function NumOfBallsChosen(val) {
-    document.getElementById('NumOfBallsChosen').innerHTML = val;
-}
-
-function validateGameTime(time) {
-    return time < 60 ? false : true;
-}
-
-/**
- * This function gets the keys the user press for movment and show them to the user
- */
-$(document).ready(function () {
-	setKeyPressed("#keyup");
-	setKeyPressed("#keydown");
-	setKeyPressed("#keyleft");
-	setKeyPressed("#keyright");
-});
-
-/**
- * This function sets a listener for each user input key to show the user the key he pressed
- * @param {*} keyIdToSet key input id to set the listener to
- */
-function setKeyPressed(keyIdToSet) {
-	$(keyIdToSet).keydown(function (event) {
-		$(keyIdToSet).attr("placeholder", event.key);
-		$(keyIdToSet).val(event.key);
-	});
-}
 
 
 
